fa6bfede91f14c5369ea9b1a845e777166b23ca6
Lengalei <ruthlengalei@gmail.com>
2024-06-12 13:49:53 +0300

    tailwind test

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,19 +1,32 @@
 import { useState } from "react";
 
 const Add = () => {
-    const [count,setCount] = useState(0)
-    
-    return ( 
-        <div className="trial">
-            <h1>count: {count}</h1>
-            <button onClick={() => {
-                setCount(count -1)
-            }}>Decreament</button>
-            <button onClick={() => {
-                setCount(count + 1)
-            }}>Increament</button>
-        </div>
-     );
-}
- 
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="trial">
+      <h1>count: {count}</h1>
+      <div className="flex gap-4" >
+        <button className="my-8 h-12 w-40 bg-red-200 rounded-2xl hover:bg-slate-100"
+          onClick={() => {
+            setCount(count - 1);
+          }}
+        >
+          Decreament
+        </button>
+
+        <button className="my-8 h-12 w-40 bg-red-200 rounded-2xl"
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increament
+      </button>
+      </div>
+
+      
+    </div>
+  );
+};
+
 export default Add;
